b44609f6cb0ccc2991cd9d2c67121bdbe9342512
0Teken6 <timu.rev006@gmail.com>
2024-06-22 01:22:11 +0600

    reqs

--- mainapp/mainapp/settings.py
+++ mainapp/mainapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fs@t$$#kcgmy*ok2orwyk!&9tdrm(^w+dvvqkdn9b=914_i+f4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
